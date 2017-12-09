1dc9aead90126f6c232406b88904a8953bf2bb0c
Anurag Patil <aspanurag@gmail.com>
2017-12-08 20:01:18 -0500

    Completed Mongoose-MongoDB and Completed- Authentication Cookie-based Session

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 
 const keys = require("../config/keys");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -12,9 +24,17 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       //from /auth/google/callback
-      console.log("access token: ", accessToken);
-      console.log("refresh token: ", refreshToken);
-      console.log("profile: ", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );

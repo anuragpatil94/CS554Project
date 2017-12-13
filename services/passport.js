5aad30cc59a0b2bf7bdd711874a0800600191afc
Anurag Patil <aspanurag@gmail.com>
2017-12-13 18:06:57 -0500

    Added new lib-Added <App/> Component

--- services/passport.js
+++ services/passport.js
@@ -16,26 +16,26 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-  new GoogleStrategy(
-    {
+  new GoogleStrategy({
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async(accessToken, refreshToken, profile, done) => {
       //from /auth/google/callback
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id
-          })
-            .save()
-            .then(user => done(null, user));
-        }
+      const existingUser = await User.findOne({
+        googleId: profile.id
       });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+          googleId: profile.id
+        })
+        .save();
+      done(null, user);
+
     }
   )
 );
